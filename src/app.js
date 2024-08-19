6ac524496f5fb0a738019143fd3888919a8543ab
lukby2457 <kjh7442455@gmail.com>
2024-08-19 17:06:37 +0900

    개발자 내용 추가

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import { add } from './add.js';
 
 const App = () => {
-  const word = add("Hello ", "No-CRA React!");
+  const word1 = add("Hello ", "No-CRA React!");
+  const word2 = add("챌린지반 ", "김진형");
   const apiUrl = process.env.APP_API_URL;
 
   return (
     <>
       <h1>챌린지반 CRA 없이 React 만들기 입니다.</h1>
       <div>
-        {word}<br />
+        {word1}<br />
+        {word2}<br />
         address : {apiUrl}
       </div>
     </>
